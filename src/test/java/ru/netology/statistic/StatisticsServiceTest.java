7451a3e3c7ba8852b551d713dfc448a350b9eb29
Yulia Zimina <yxalzova@gmail.com>
2025-10-27 20:28:53 +0400

    counter

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxIfB() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-9, -5, -4, -7, 0, -10, -11, 0, -5, -4, -1};
+        long expected = 0;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
